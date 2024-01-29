a97254602bc073cb9ef3f45974e0e7b788a79787
petrero <petrero@gmail.com>
2024-01-30 00:16:30 +0200

    6.2 UI Input and Button  - Just like in the Text Analyze feature, we’ll have an input that is bound to the question the user asks using v-model. And it will have a button to click to submit.
     The submit button will trigger an action in the audioChat store called createPrompt , which will build the prompt and invoke the sendPrompt action.

---
+++ src/components/ChatWindowAudio.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { useAudioChatStore } from "@/stores/audioChat.js"
+
+  const audioChatStore = useAudioChatStore()
+</script>
+
+<template>
+  <div>
+    <div class="flex rounded-md mt-4">
+      <div class="relative flex flex-col flex-grow items-strech">
+        <div>
+          <div class="flex shadow-sm mb-4"></div>
+          <!-- input and submit button -->
+          <input
+              v-model="audioChatStore.question"
+              class="question-input"
+              placeholder="Send a message"
+          />
+          <button
+              type="button"
+              @click="audioChatStore.createPrompt()"
+              class="chat-button group relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 text-sm font-semibold bg-[#1a1a21] text-green-500 ring-1 ring-inset ring-gray-300"
+          >
+            <span class="text-green-100 group-hover:bg-[#42b983]">submit</span>
+          </button>
+        </div>
+        <!-- Chat UI -->
+      </div>
+    </div>
+  </div>
+</template>
