2dc2f8508c283c6f04662a8b008d6aff1b2e624a
petrero <petrero@gmail.com>
2024-01-29 22:45:06 +0200

    2.7 Remove unnecessary code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="flex flex-col items-center mt-40">
+    
+  </section>
 </template>
