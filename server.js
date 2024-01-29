778ea742c3a256aea53b563fa20538e0de8dff66
petrero <petrero@gmail.com>
2024-01-30 00:01:41 +0200

    5.9 Now we’ll configure multer in the server. We’ll bring in the upload method.
     Configure Deepgram
     - We already installed the Deepgram SDK, but now that we’re in the server file, let’s configure it with our API Key
    
     Endpoint /dg-transcription
     - Now, let’s create a server endpoint called /dg-transcription. This endpoint will handle our interaction with Deepgram. We’ll also use Multer as middleware for handling the single file sent back to our server.
    
     Pay special attention to the string ‘file’ we give to upload.single('file'). The name of this string (which we’ve called ‘file’) will need to match the name attribute we used on the uploader button on our frontend. Multer will not work if this string passed to the upload.single method does not match the name attribute of the uploader on the frontend.
    
     The logic inside this endpoint will make the request to Deepgram, sending the file buffer of the audio file we want to transcribe along with the mimetype of the audio. The mimetype is just the type of file, such as audio/wav .
    
     We can also give the Deepgram API options to improve the transcription. We can set punctuate:true so that the transcription contains formatting like punctuation and capitalization. We can also tell it which model to use. I’ll use model:nova because that’s currently Deepgram’s most performant model for speech-to-text.
    
     There are lots of other options to format and improve the transcription, so I recommend checking them out to see all the things you can do with Deepgram’s API!

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const envConfig = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const multer = require('multer')
 
+const upload = multer()
 const port = 3000
 const app = express()
 app.use(cors())
@@ -56,6 +58,27 @@ app.post('/tokenize', async (req, res) => {
     })
   } catch(error) {
     console.log(error.message)
+  }
+})
+
+const { Deepgram } = require('@deepgram/sdk')
+const deepgram = new Deepgram(process.env.DG_API)
+
+app.post('/dg-transcription', upload.single('file'), async (req, res) => {
+  try {
+    const dgResponse = await deepgram.transcription.preRecorded(
+      {
+        buffer: req.file.buffer,
+        mimetype: req.file.mimetype
+      },
+      {
+        punctuate: true,
+        model: 'nova'
+      }
+    )
+    res.send({transcript: dgResponse})
+  } catch (e) {
+    console.log('error', e)
   }
 })
 
