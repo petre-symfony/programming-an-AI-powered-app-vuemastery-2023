ac9eaee2c3f84891602544f3741189f7de7e0595
petrero <petrero@gmail.com>
2024-01-29 23:47:52 +0200

    5.4 Then we’ll write the initial code we need for the file uploader. The open method from useFileDialog will make the file dialog appear so that the user can select a file.
     Pay attention to the “name” attribute on the “Choose file” button. I specifically placed that attribute; it’s not something related to the VueUse composable. That attribute will be important later when we use a package called Multer in our Node server to handle files uploaded to our server.
    
     We’ll need to keep track of this file that has been uploaded because it’s the audio file we intend to send to Deepgram. Since we have quite a bit of logic related to this Analyze Audio feature, and we want to keep all our logic clearly separated to help with learning these technologies, let’s create a Pinia store where we can write that logic.

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,7 +1,41 @@
 <script setup>
+  import { useFileDialog } from "@vueuse/core"
+  const { files, open, reset, onChange } = useFileDialog()
 
+  function resetFile() {
+    reset()
+  }
 </script>
 
 <template>
+  <div class="flex flex-col h-36">
+    <div class="flex">
+      <button
+          type="button"
+          :disabled="!files"
+          @click="open()"
+          class="button button-primary w-36 mr-2">
+        Choose File
+      </button>
+      <button
+          type="button"
+          name="file"
+          :disabled="!files"
+          @click="resetFile()"
+          class="button-secondary w-36"
+      >
+        Reset
+      </button>
+    </div>
 
+    <div class="mt-6" v-if="files">
+      <li
+          class="list-none font-semibold my-2 text-purple-300"
+          v-for="file of files"
+          :key="file.name"
+      >
+        {{ file.name }}
+      </li>
+    </div>
+  </div>
 </template>
