9afbb124d9fcdfb65a558e0b460c3604c86a0267
petrero <petrero@gmail.com>
2024-01-31 18:08:20 +0200

    8.3 Backend /miniGPT endpoint  - In the server, we’ll add the configuration for Replicate.
     All models accessed with the Replicate API can be run using the replicate.run method. This method expects us to give it the model name and and object that contains a property called input. That input property has a value that is an object.
    
     Because miniGPT expects an image, one of the input properties will be named image. The value of this property will be the image url string.
    
     The model also expects the prompt. We don’t need to send in high-level instructions like we did with OpenAI, since this model is already trained to do exactly what we want it to do - have a conversation about an image we give it. So the prompt we send in will be the question asked by the user.
    
     Here is our logic to make Replicate run the miniGPT model

--- server.js
+++ server.js
@@ -125,4 +125,24 @@ app.get('/clear-chain', async (req, res) => {
   })
 })
 
+const Replicate = require('replicate')
+const replicate = new Replicate({
+  auth: process.env.replicate
+})
+
+const miniGPT = 'daanelson/minigpt-4:b96a2f33cc8e4b0aa23eacfce731b9c41a7d9466d9ed4e167375587b54db9423'
+
+app.post('/minigpt', async (req, res) => {
+  try {
+    const miniGPTResponse = await replicate.run(miniGPT, {
+      input: {
+        image: req.body.image,
+        req: req.body.prompt
+      }
+    })
+    res.send({ message: miniGPTResponse})
+  } catch (e) {
+    console.log('error', e)
+  }
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
